d4766f65a1cf9f64d3810c4c9bd03080520bcd17
Srilekha Jannabhatla <srilekha.jannabhatla9@gmail.com>
2024-07-23 20:34:35 -0400

    Removed HomeController and added FoodController, added view to the viewall action of FoodController, Changed the _layout page

---
+++ VeganRecipes/Controllers/FoodController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VeganRecipes.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+    }
+}
